3b24f53f323b206789df0ef8d2ec64331b053295
Sympa <Ihre_EMailadresse>
2025-01-11 18:20:21 +0100

    add qr bild & user input text

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import qrcode
 import tkinter as tk
-from tkinter import ttk, messagebox, colorchooser, filedialog, PhotoImage
+from tkinter import ttk, messagebox, colorchooser, filedialog
 from PIL import Image, ImageTk
 
 class QrCodeGenerieren:
@@ -31,18 +31,21 @@ class QrCodeGenerieren:
 
 # TODO: später löschen
 qr_data = "Hallo Welt!"
-fl_color = "Green"
-bg_color = "Black"
+fl_color = "Black"
+bg_color = "White"
 t = QrCodeGenerieren(qr_data, fl_color, bg_color)
 t.qr_erzeugen()
 
 
 # TODO: GUI Klasse hier
 class Gui(tk.Tk):
+    """Die Klasse GUI erbt von der TKinter Klasse und beinhaltet alle GUI Komponenten.
+    """
     def __init__(self):
         super().__init__()
 
         self.title("QR-Code Generator")
+        # self.geometry("400x430")
 
         # Icon laden und in ein unterstütztes Format konvertieren um Linux kompatiblität zu ermöglichen
         self.icon_image = Image.open("qr-code-outline.ico")
@@ -55,7 +58,24 @@ class Gui(tk.Tk):
         self.gui_widgets()
 
     def gui_widgets(self):
-        pass
+        """ Funktion um alle GUI Komponenten zu erstellen.
+        """
+
+        # LÄd den QR mit dem GitHub Repo Link & zeigt diesen an
+        self.qr_image = Image.open("qrcode_gui.png")
+        self.qr_image.thumbnail((200, 200))
+        self.qr_photo = ImageTk.PhotoImage(self.qr_image)
+        self.qr_label = ttk.Label(self, image=self.qr_photo)
+        self.qr_label.pack(pady=20)
+
+        # Erstellt den Rahmen für die Nutzereingaben
+        self.user_eingaben_lfrm = ttk.Labelframe(self, text="QR-Code Einstellung")
+        self.user_eingaben_lfrm.pack(pady=20)
+        # QR-Code Texteingabe
+        self.user_entr_lfrm = ttk.Labelframe(self.user_eingaben_lfrm, text="QR-Code Text")
+        self.user_entr_lfrm.pack(pady=10, padx=10)
+        self.user_entr = ttk.Entry(self.user_entr_lfrm, width="60")
+        self.user_entr.pack(pady=5, padx=5)
 
 
 def main():
