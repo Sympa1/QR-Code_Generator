6a1e241555f7802308fd4815b74a76d927345be4
sympa <sympa>
2025-01-12 10:27:13 +0100

    add datei fehlerbehandlung, farbdartstellung, qr code aktualisierung im gui & qr code erstellung

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ from tkinter import ttk, messagebox, colorchooser, filedialog
 from PIL import Image, ImageTk
 import os
 
-# TODO: Add wrapper FUnktion um die eigentliche Logik auszuführen // Add Option um die aktuellen Einstellungen anzuzeigen (Farbe und Speicherort)
-
 class QrCodeGenerieren:
     """Diese Klasse erzeugt die QR Codes. Data = Inhalt des QR Codes | fl_color = Füllfarbe | bg_color = Hintergrundfarbe"""
 
-    def __init__(self, data: str, fl_color, bg_color):
+    def __init__(self, data: str, fl_color: str, bg_color: str, pfad: str):
         # Objekt der Bibliotheksklasse "QRCode" als Klassenattribut speichern
         self.qr = qrcode.QRCode(box_size=10, border=2)
         self.data = data
         self.fl_color = fl_color
         self.bg_color = bg_color
+        self.pfad = pfad
 
     def qr_erzeugen(self):
         # Datensatz hinzufügen
@@ -27,17 +26,9 @@ class QrCodeGenerieren:
         img = self.qr.make_image(fill_color=self.fl_color, back_color=self.bg_color)
 
         # Bild speichern
-        img.save("qrcode.png")
+        img.save(self.pfad)
 
         print("QR-Code erfolgreich generiert und als 'qrcode.png' gespeichert.")
-
-
-# TODO: später löschen
-qr_data = "Hallo Welt!"
-fl_color = "Black"
-bg_color = "White"
-t = QrCodeGenerieren(qr_data, fl_color, bg_color)
-t.qr_erzeugen()
 
 
 # TODO: GUI Klasse hier
@@ -56,9 +47,9 @@ class Gui(tk.Tk):
         self.iconphoto(False, self.icon_photo)
 
         #Deklaration der Hilfsvariablen
-        self.farbwahl_fuell = None
-        self.farbwahl_bg = None
-        self.dateipfad = None
+        self.farbwahl_fuell = "Black"
+        self.farbwahl_bg = "White"
+        self.dateipfad = self.user_verzeichnis()
 
         self.gui_widgets()
 
@@ -96,10 +87,28 @@ class Gui(tk.Tk):
         self.fuell_btn = ttk.Button(self.farbwahl_lfrm, text=" Durchsuchen ", command=lambda: self.speicherort())
         self.fuell_btn.pack(pady=5, padx=5)
 
+        # Darstellung der Usereinstellungen
+        self.usereinstellungen_lfrm = ttk.Labelframe(self, text="Gewählte Einstellungen")
+        self.usereinstellungen_lfrm.pack(pady=(20, 0), padx=10)
+        self.usereinstellungen_fuell_label = ttk.Label(self.usereinstellungen_lfrm, text="Gewählte Füllfarbe: ")
+        self.usereinstellungen_fuell_label.grid(row=0, column=0, pady=5, padx=5, sticky="w")
+        self.usereinstellungen_fuell_label_farbe = ttk.Label(self.usereinstellungen_lfrm, text="      ", background=self.farbwahl_fuell)
+        self.usereinstellungen_fuell_label_farbe.grid(row=0, column=1, pady=5, padx=5)
+        
+        self.usereinstellungen_bg_label = ttk.Label(self.usereinstellungen_lfrm, text="Gewählte Hintergrundfarbe: ")
+        self.usereinstellungen_bg_label.grid(row=1, column=0, pady=5, padx=5)
+        self.usereinstellungen_bg_label_farbe = ttk.Label(self.usereinstellungen_lfrm, text="      ", background=self.farbwahl_bg)
+        self.usereinstellungen_bg_label_farbe.grid(row=1, column=1, pady=5, padx=5)
+
+        self.usereinstellungen_pfad_label = ttk.Label(self.usereinstellungen_lfrm, text="Gewählter Speicherort:")
+        self.usereinstellungen_pfad_label.grid(row=2, column=0, pady=5, padx=5, sticky="w")
+        self.usereinstellungen_pfad_label_text = ttk.Label(self.usereinstellungen_lfrm, text=self.user_verzeichnis())
+        self.usereinstellungen_pfad_label_text.grid(row=2, column=1, pady=5, padx=5)
+
         # Steuerbuttons
-        self.btn_frm = ttk.Frame(self)
+        self.btn_frm = ttk.Frame(self, width=60)
         self.btn_frm.pack(pady=10, side="right")
-        self.erstelle_qr_btn = ttk.Button(self.btn_frm, text="QR-Code Generieren", command=self.erstelle_qr())
+        self.erstelle_qr_btn = ttk.Button(self.btn_frm, text="QR-Code Generieren", command=lambda: self.erstelle_qr())
         self.erstelle_qr_btn.pack(side="left", padx=5, pady=10)
         self.abbrechen_btn = ttk.Button(self.btn_frm, text="Abbrechen", command=lambda: self.destroy())
         self.abbrechen_btn.pack(side="right", padx=(5, 10), pady=10)
@@ -112,12 +121,15 @@ class Gui(tk.Tk):
         if btn_id == 1:
             farbe = colorchooser.askcolor(title="Farbauswahl")
             self.farbwahl_fuell = farbe[1]
+            self.usereinstellungen_fuell_label_farbe.config(background=self.farbwahl_fuell)
         elif btn_id == 2:
             farbe = colorchooser.askcolor(title="Farbauswahl")
             self.farbwahl_bg = farbe[1]
+            self.usereinstellungen_bg_label_farbe.config(background=self.farbwahl_bg)
 
 
-    def speicherort(self):
+
+    def user_verzeichnis(self):
         # Dynamisch das Benutzerverzeichnis ermitteln
         benutzer_verzeichnis = os.path.expanduser("~")
         
@@ -131,6 +143,11 @@ class Gui(tk.Tk):
             if os.path.exists(test_pfad):
                 initial_verzeichnis = test_pfad
                 break
+        return initial_verzeichnis + "/qrcode.png"
+
+
+    def speicherort(self):
+        initial_verzeichnis = self.user_verzeichnis()
         
         # Öffnet den Dateidialog zum Speichern der Datei
         self.dateipfad = filedialog.asksaveasfilename(
@@ -145,10 +162,29 @@ class Gui(tk.Tk):
             ],
             title="Speicherort auswählen"
         )
+        self.usereinstellungen_pfad_label_text.config(text=self.dateipfad)
 
-    
+
     def erstelle_qr(self):
-        pass
+        if os.path.exists(self.dateipfad):
+            if messagebox.askyesnocancel("Speicherort vergeben", "Der gewählte Speicherort existiert bereits."):
+                ueberschreibe = True
+        else:
+            ueberschreibe = True
+        
+        if ueberschreibe:
+            self.data = self.user_entr.get()
+            qr = QrCodeGenerieren(self.data, self.farbwahl_fuell, self.farbwahl_bg, self.dateipfad)
+            qr.qr_erzeugen()
+            if os.path.exists(self.dateipfad):
+                messagebox.showinfo("Erfolg!", "Der QR-Code wurde Erfolgreich gespeichert.")
+                self.qr_image = Image.open(self.dateipfad)
+                self.qr_image.thumbnail((200, 200))
+                self.qr_photo = ImageTk.PhotoImage(self.qr_image)
+                self.qr_label.config(image=self.qr_photo)
+
+            else:
+                messagebox.showerror("Fehler!", "Der QR-Code konnte nicht gespeichert werden.")
             
 
 def main():
